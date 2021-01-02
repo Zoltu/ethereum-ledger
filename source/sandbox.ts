c78563136193d3ff1706788e898d3ad9ef138408
Micah Zoltu <micah@zoltu.net>
2021-01-02 13:55:47 +0800

    Adds build output for hosting the sandbox via IPFS.
    Including a few usability improvements like error handling.  The idea is to publish to IPFS so it is easy to quickly test a ledger.

--- source/sandbox.ts
+++ source/sandbox.ts
@@ -6,7 +6,7 @@ const tokensArray = [
 	["ethereum","augur","REP",18,"Augur","3045022100ae8fe2f0e9f2b2ba551277801aea9ca52d064a37e086e4f71020f694ef97b33f022031a5d6ca5545252a34af01e83db83c1ba95918e61da0ca72d7464aad4c8fd9f1","0x1985365e9f78359a9B6AD760e32412f4a445E862",false,false],
 	["ethereum","makerdao","MKR",18,"MakerDAO","304402200bca467156035534a4fa8aeafff967b3845fc3cc11f6eea446d283103d8d23f602206eb2e6a90dd67b0bc45a8660ef501c560952c671c32f7394ac23a74191ac4f31","0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2",false,false],
 ] as const
-const tokens = tokensArray.reduce((aggregate, [,, symbol, decimals,, signatureHex, addressHex]) => {
+const tokens = tokensArray.reduce((aggregate, [,, symbol, decimals, name, signatureHex, addressHex]) => {
 	if (aggregate[symbol] !== undefined) console.warn(`Duplicate token ${symbol}.`)
 	const signature = uint8ArrayFromHexString(signatureHex)
 	const address = uint8ArrayFromHexString(addressHex) as Uint8Array & {length:20}
@@ -15,30 +15,56 @@ const tokens = tokensArray.reduce((aggregate, [,, symbol, decimals,, signatureHe
 }, {} as { [key: string]: { symbol: string, decimals: number, name: string, signature: Uint8Array, address: Uint8Array & {length:20} } })
 
 let state = 0
+
+function setHighlight() {
+	document.getElementById('address')!.parentElement!.style.backgroundColor = 'unset'
+	document.getElementById('version')!.parentElement!.style.backgroundColor = 'unset'
+	document.getElementById('signature')!.parentElement!.style.backgroundColor = 'unset'
+	document.getElementById(state === 0 ? 'address' : state === 1 ? 'version' : 'signature')!.parentElement!.style.backgroundColor = 'yellow'
+}
+setHighlight()
+
+function reset() {
+	state = 0
+	document.getElementById('address')!.innerText = ''
+	document.getElementById('version')!.innerText = ''
+	document.getElementById('signature')!.innerText = ''
+	document.getElementById('error')!.innerText = ''
+	setHighlight()
+}
+(window as any).derivationPathChangedHandler = reset
+
 async function clickHandler() {
-	switch (state) {
-		case 0:
-			state = 1
-			const address = await getAddress()
-			const addressString = address.toString(16)
-			document.getElementById('address')!.innerText = addressString
-			break
-		case 1:
-			state = 2
-			const appConfiguration = await getAppConfiguration()
-			document.getElementById('version')!.innerText = `${appConfiguration.majorVersion}.${appConfiguration.minorVersion}.${appConfiguration.patchVersion}`
-			break
-		case 2:
-			state = 0
-			const token = tokens['DAI']
-			// NOTE: `await` intentionally missing on this call because awaiting it causes Firefox to think that the user interaction chain ends when this call returns. by not awaiting it, we can instead rely on the internal locking system to properly queue this up with the following `signTransaction`, thus causing Firefox to think both APDU calls are part of the same user interaction
-			provideErc20TokenInformation(token.symbol, token.address, token.decimals, 1, token.signature)
-			const signature = await signTransaction(uint8ArrayFromHexString(`f8aa80850430e2340083030d409489d24a6b4ccb1b6faa2625fe562bdd9a2326035980b844a9059cbb000000000000000000000000${document.getElementById('address')!.innerText}00000000000000000000000000000000000000000000000098a7d9b8314c0000010000`))
-			const rString = signature.r.toString(16)
-			const sString = signature.s.toString(16)
-			const vString = signature.v.toString(16)
-			document.getElementById('signature')!.innerText = `{ r: "${rString}", s: "${sString}", v: "${vString}" }`
-			break
+	try {
+		const derivationPath = (document.getElementById('derivation-path')! as HTMLInputElement).value || `m/44'/60'/0'/0/0`
+		switch (state) {
+			case 0:
+				const address = await getAddress(derivationPath)
+				const addressString = address.toString(16)
+				document.getElementById('address')!.innerText = addressString
+				state = 1
+				break
+			case 1:
+				const appConfiguration = await getAppConfiguration()
+				document.getElementById('version')!.innerText = `${appConfiguration.majorVersion}.${appConfiguration.minorVersion}.${appConfiguration.patchVersion}`
+				state = 2
+				break
+			case 2:
+				const token = tokens['DAI']
+				// NOTE: `await` intentionally missing on this call because awaiting it causes Firefox to think that the user interaction chain ends when this call returns. by not awaiting it, we can instead rely on the internal locking system to properly queue this up with the following `signTransaction`, thus causing Firefox to think both APDU calls are part of the same user interaction
+				provideErc20TokenInformation(token.symbol, token.address, token.decimals, 1, token.signature)
+				const signature = await signTransaction(uint8ArrayFromHexString(`f8aa80850430e2340083030d409489d24a6b4ccb1b6faa2625fe562bdd9a2326035980b844a9059cbb000000000000000000000000${document.getElementById('address')!.innerText}00000000000000000000000000000000000000000000000098a7d9b8314c0000010000`))
+				const rString = signature.r.toString(16)
+				const sString = signature.s.toString(16)
+				const vString = signature.v.toString(16)
+				document.getElementById('signature')!.innerText = `{ r: "${rString}", s: "${sString}", v: "${vString}" }`
+				state = 0
+				break
+			}
+		setHighlight()
+	} catch (error) {
+		reset()
+		document.getElementById('error')!.innerText = error.message
 	}
 }
 (window as any).clickHandler = clickHandler
